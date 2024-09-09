c3b6f55bc13c51520970add44037c0d628ca20f5
Festus Kiprotich <m1ndnyong1@gmail.com>
2024-09-09 04:04:26 -0700

    modified:   python_introduction/future_age_calculator.py

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,3 +1,2 @@
-age = int(input("How old are you?: "))  # Prompt the user for their age
-print("In 2050, you will be", age + 23, "years old.")  # Calculate future age
-
+age = input("How old are you?: ")  # Prompt the user for their age
+print("In 2050, you will be",int(age) + 23, "years old.")  # Calculate future age
