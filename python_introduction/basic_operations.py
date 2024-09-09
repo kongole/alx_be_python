fbe7a133c1d6de36c59323a33df1e812fb3e1e5f
Festus Kiprotich <m1ndnyong1@gmail.com>
2024-09-09 02:38:45 -0700

    modified:   python_introduction/basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -5,5 +5,5 @@ difference = number1 - number2
 product = number1 * number2
 
 print("Addition of", number1, "and", number2, "is", sum)
-print("Subtraction of", number1, "and", number2, "is",difference)
+print("Subtraction of", number1, "and", number2, "is", difference)
 print("Multiplication of", number1, "and", number2, "is", product)
