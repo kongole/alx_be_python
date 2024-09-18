3828ed303fd9f7fe3fe48686d209ce14b50f9450
Festus Kiprotich <m1ndnyong1@gmail.com>
2024-09-18 04:29:32 -0700

    Fixed syntax errors in weather.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,9 +1,9 @@
 weather = input("What's the weather like today? (sunny/rainy/cold): ")
-if input = sunny
-   print("Wear a t-shirt and sunglasses.")
-   elif input = rainy
-        print ("Don't forget your umbrella and a raincoat.")
-    elif  input = cold
-             print("Make sure to wear a warm coat and a scarf.")
-         else input == weather
-                   print("Sorry, I don't have recommendations for this weather.")
+if input == "sunny":
+ print("Wear a t-shirt and sunglasses.")
+elif input == "rainy":
+ print ("Don't forget your umbrella and a raincoat.")
+elif  input == "cold":
+ print("Make sure to wear a warm coat and a scarf.")
+else:
+ print("Sorry, I don't have recommendations for this weather.")
